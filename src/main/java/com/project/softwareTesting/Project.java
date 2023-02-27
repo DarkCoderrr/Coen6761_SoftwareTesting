7057de9eb6cfba74387908a94e881e3e5cd7e1a0
abhimadhu <92879492+abhimadhu@users.noreply.github.com>
2023-02-26 20:11:54 -0500

    Code changes for task 2
    Code changes for task 2

--- src/main/java/com/project/softwareTesting/Project.java
+++ src/main/java/com/project/softwareTesting/Project.java
@@ -1,23 +1,21 @@
 package com.project.softwareTesting;
 
+
+
 import java.util.Scanner;
 
 public class Project {
 
 	int N;
-	int[][] floor = new int[N][N];
-	int Xcoordinate = N - 1;
-	int Ycoordinate = 0;
-
-	String facing = "north";
+	int[][] floor ;
+	int Xcoordinate;
+	int Ycoordinate;
+	String facing;
 	boolean pen;
-	String penValueString = "Up";
+	String penValueString;
+	boolean movepen=true;
 
-	public Project(int n) {
-		this.N = n;
-	}
-
-	public int getXcoordinate() {
+	public int getXcoordinate() { 
 		return Xcoordinate;
 	}
 
@@ -27,10 +25,6 @@ public class Project {
 
 	public int getN() {
 		return N;
-	}
-
-	public void setMatrix(int[][] matrix) {
-		this.floor = matrix;
 	}
 
 	public void setXcoordinate(int xcoordinate) {
@@ -52,70 +46,103 @@ public class Project {
 		this.penValueString = "UP";
 		return false;
 	}
+	 public void initialize(int newSize) {
+	        this.N = newSize;
+	        this.floor = new int[N][N];
+	        setXcoordinate(N-1);
+	        setYcoordinate(0);
+//	        this.Xcoordinate = N-1;
+//	        this.Ycoordinate = 0;
+	        penUp();
+	        this.facing = "North";
+	    }
 
-//	public int[][] initializeFloor(int size) {
-//		int matrix[][]=new int[size][size];
-//		for(int i=0;i<size;i++) {
-//			for(int j=0;j<size;j++) {
-//				matrix[i][j]=0;
-//			}
-//		}
-//		this.floor=matrix;
-//		this.N=N;
-//		return matrix;
-//	}
-	public void InputCommand() {
-
-		for (int i = 0; i < N * N; i++) {
+	public char InputCommand() {
+       
+	 char returncommand='X';
+		Scanner objScanner = new Scanner(System.in);
+		boolean running = true;
+		while(running==true){
 			System.out.print("Enter one of the above Command : ");
-			Scanner objScanner = new Scanner(System.in);
+			
 			String inputCommandString = objScanner.nextLine();
-
-			if (inputCommandString.equalsIgnoreCase("U") || inputCommandString.equalsIgnoreCase("u")) {
-				penUp();
-			} else if (inputCommandString.equalsIgnoreCase("D") || inputCommandString.equalsIgnoreCase("d")) {
-				penDown();
-			} else if (inputCommandString.equalsIgnoreCase("R") || inputCommandString.equalsIgnoreCase("r")) {
-				TurnRight();
-			} else if (inputCommandString.equalsIgnoreCase("L") || inputCommandString.equalsIgnoreCase("l")) {
-				TurnLeft();
-			} else if (inputCommandString.equalsIgnoreCase("C") || inputCommandString.equalsIgnoreCase("c")) {
-				PrintCurrentLocation();
-			} else if (inputCommandString.equalsIgnoreCase("P") || inputCommandString.equalsIgnoreCase("p")) {
-				print();
-			} else if (inputCommandString.equalsIgnoreCase("Q") || inputCommandString.equalsIgnoreCase("q")) {
-				break;
-			} else {
-				if (inputCommandString.charAt(0) == 'M' || inputCommandString.charAt(0) == 'm') {
-					String digits[] = inputCommandString.split(" ");
-					int distance = Integer.parseInt(digits[1]);
-					Move(distance);
-				}
-			}
+			char command = inputCommandString.charAt(0);
+			returncommand=command;
+			
+			switch (command) {
+            case 'U':
+            case 'u':
+                penUp(); 
+                break;
+            case 'D':
+            case 'd':
+                penDown();
+                break;
+            case 'R':
+            case 'r':
+            	turnRight();            	
+                break;
+            case 'L':
+            case 'l':
+            	turnLeft();
+                break;
+            case 'M':
+            case 'm':
+                String[] parts = inputCommandString.split(" ");
+                int distance = Integer.parseInt(parts[1]);
+                moveForwardBackward(distance);
+                break;
+            case 'P':
+            case 'p':
+            	print();
+                break;
+            case 'C':
+            case 'c':
+            	PrintCurrentLocation();
+                break;
+            case 'I':
+            case 'i':
+                String[] sizeParts = inputCommandString.split(" ");
+                int newSize = Integer.parseInt(sizeParts[1]);
+                initialize(newSize);
+                break;
+            case 'Q':
+            case 'q':
+                running = false;
+                break;
+            default:
+                System.out.println("Invalid command");
+                break;
+        }
+	
 		}
-
+		objScanner.close();
+		return returncommand;
 	}
-
+	
 	public int[] PrintCurrentLocation() {
 		int coordinates[] = new int[2];
-		coordinates[0] = Xcoordinate;
+		int updatedX=this.N-Xcoordinate-1;
+		coordinates[0] = updatedX;
 		coordinates[1] = Ycoordinate;
-		System.out.println(">Position: " + Xcoordinate + " , " + Ycoordinate + "- Pen: " + penValueString
+		System.out.println(">Position: " + updatedX + " , " + Ycoordinate + "- Pen: " + penValueString
 				+ " - Facing: " + facing);
 		return coordinates;
 
 	}
 
 	public boolean BoundaryConditiion(int distance) {
-		if ((facing.equals("east") && (distance <= (N - Ycoordinate)))
-				|| (facing.equals("west") && (distance <= Ycoordinate))
-				|| (facing.equals("north") && (distance <= Xcoordinate))
-				|| (facing.equals("south") && (distance <= (N - Xcoordinate))))
+		if ((this.facing=="East" && (this.Ycoordinate + distance < this.N))
+				|| (this.facing=="West"&& (this.Ycoordinate - distance >= 0))
+				|| (this.facing=="North" && (this.Xcoordinate - distance >= 0))
+				|| (this.facing=="South" && (this.Xcoordinate + distance < this.N)))
 
 		{
-			return true;
+			this.movepen=true;
+			return movepen;
 		} else {
-			return false;
+			this.movepen=false;
+			return movepen;
 		}
 
 	}
@@ -124,78 +151,136 @@ public class Project {
 		return facing;
 	}
 
-	public void TurnRight() {
-		if (facing.equals("north")) {
-			facing = "east";
-		} else if (facing.equals("east")) {
-			facing = "south";
-		} else if (facing.equals("south")) {
-			facing = "west";
-		} else if (facing.equals("west")) {
-			facing = "north";
+	public void turnRight() {
+		if (this.facing=="North") {
+			setFacing("East");
+		} else if (this.facing=="East") {
+			setFacing("South");
+		} else if (this.facing=="South") {
+			setFacing("West");
+		} else if (this.facing=="West") {
+			setFacing("North");
 		}
 
 	}
 
-	public void TurnLeft() {
-		if (facing.equals("north")) {
-			facing = "west";
-		} else if (facing.equals("west")) {
-			facing = "south";
-		} else if (facing.equals("south")) {
-			facing = "east";
-		} else if (facing.equals("east")) {
-			facing = "north";
+
+	public void turnLeft() {
+		if (this.facing=="North") {
+			setFacing("West");
+		} else if (this.facing=="West") {
+			setFacing("South");
+		} else if (this.facing=="South") {
+			setFacing("East");
+		} else if (this.facing=="East") {
+			setFacing("North");
 		}
 
 	}
 
-	public void Move(int distance) {
 
-		for (int j = distance; j > 0; j--) {
-
-			if (facing.equals("north") && pen && BoundaryConditiion(j)) {
-				floor[Xcoordinate][Ycoordinate] = 1;
-				Xcoordinate--;
-			}
-
-			else if (facing.equals("east") && pen && BoundaryConditiion(j)) {
-				floor[Xcoordinate][Ycoordinate] = 1;
-				Ycoordinate++;
-			}
-
-			else if (facing.equals("south") && pen && BoundaryConditiion(j)) {
-				floor[Xcoordinate][Ycoordinate] = 1;
-				Xcoordinate++;
-			}
-
-			else if (facing.equals("west") && pen && BoundaryConditiion(j)) {
-				floor[Xcoordinate][Ycoordinate] = 1;
-				Ycoordinate--;
-			}
-
-			else if (!pen) {
-				floor[Xcoordinate][Ycoordinate] = 2;
-				if (facing.equals("north")) {
-					Xcoordinate--;
-				} else if (facing.equals("east")) {
-					Ycoordinate++;
-				} else if (facing.equals("south")) {
-					Xcoordinate++;
-				} else {
-					Ycoordinate--;
-				}
-			}
-
-			else if (!BoundaryConditiion(distance)) {
-				System.out.println("Cannot move outside of floor boundaries.");
-			}
-		}
-	}
-
-	public void setN(int n) {
-		N = n;
-	}
+	
+	 public void moveForwardBackward(int distance) {
+		 BoundaryConditiion(distance);		 
+	        switch (this.facing) {
+	            case "North":
+	                if (this.pen && movepen) {
+	                    for (int i = 1; i <= distance; i++) {
+	                        if (this.Xcoordinate - i >= 0) {
+	                            this.floor[this.Xcoordinate -i][this.Ycoordinate] = 1;
+	                        }
+	                    }
+	                }
+	                else if(!this.pen && movepen) {
+	                	for (int i = 1; i <= distance; i++) {
+	                        if (this.Xcoordinate - i >= 0) {
+	                            this.floor[this.Xcoordinate -i][this.Ycoordinate] = 0;
+	                        }
+	                    }
+	                }
+	                else {
+	                	System.out.println("Please provide Distance within Floor Space Size");
+						break;
+					}
+	                this.Xcoordinate -= distance;	                
+	                break;
+	            case "East":
+	                if (this.pen && movepen) {
+	                    for (int i = 1; i <= distance; i++) {
+	                        if (this.Ycoordinate + i < this.N) {
+	                            this.floor[this.Xcoordinate][this.Ycoordinate + i] = 1;
+	                        }
+	                    }
+	                }
+	                else if(!this.pen && movepen) {
+	         
+		                    for (int i = 1; i <= distance; i++) {
+		                        if (this.Ycoordinate + i < this.N) {
+		                            this.floor[this.Xcoordinate][this.Ycoordinate + i] = 0;
+		                        }
+		                    }
+		                }
+	                
+	                else {
+	                	System.out.println("Please provide Distance within Floor Space Size");
+						break;
+					}
+	                this.Ycoordinate += distance;
+	                break;
+	            case "South":
+	                if (this.pen&& movepen) {
+	                    for (int i = 1; i <= distance; i++) {
+	                        if (this.Xcoordinate + i < this.N) {
+	                            this.floor[this.Xcoordinate + i][this.Ycoordinate] = 1;
+	                        }
+	                    }
+	                }
+	                else if (!this.pen&& movepen) {
+	                    for (int i = 1; i <= distance; i++) {
+	                        if (this.Xcoordinate + i < this.N) {
+	                            this.floor[this.Xcoordinate + i][this.Ycoordinate] = 0;
+	                        }
+	                    }
+	                }
+	                else {
+	                	System.out.println("Please provide Distance within Floor Space Size");
+						break;
+					}
+	                this.Xcoordinate += distance;
+	                break;
+	            case "West":
+	                if (this.pen&& movepen) {
+	                    for (int i = 1; i <= distance; i++) {
+	                        if (this.Ycoordinate - i >= 0) {
+	                            this.floor[this.Xcoordinate][this.Ycoordinate - i] = 1;
+	                        }
+	                    }
+	                }
+	                else if (this.pen==false && movepen) {
+	                    for (int i = 1; i <= distance; i++) {
+	                        if (this.Ycoordinate - i >= 0) {
+	                            this.floor[this.Xcoordinate][this.Ycoordinate - i] = 0;
+	                        }
+	                    }
+	                }
+	                else {
+	                	System.out.println("Please provide Distance within Floor Space Size");
+						break;
+					}
+	                this.Ycoordinate -= distance;
+	                break;
+	            default:
+	                System.out.println("Invalid direction");
+	                break;
+	        }
+	    }
+	
+	
+	
+	
+	
+	
+	
 
 	public String[][] print() {
 		String testmatrix[][] = new String[N][N]; // Defined matrix for testing purpose
@@ -232,25 +317,12 @@ public class Project {
 				+ "is back to [0, 0], pen up and facing north. n size of the array, an integer\r\n"
 				+ "greater than zero");
 
-		System.out.println("Enter The First command to initialize Size of Array: ");
-		Scanner objScanner = new Scanner(System.in);
-		String sizeString = objScanner.nextLine();
 
-		if (sizeString.charAt(0) == 'I' || sizeString.charAt(0) == 'N' || sizeString.charAt(0) == 'i'
-				|| sizeString.charAt(0) == 'n') {
-
-			String digits[] = sizeString.split(" ");
-			int distance = Integer.parseInt(digits[1]);
-
-			Project project = new Project(distance);
-
-			int matrix[][] = new int[distance][distance];
-			project.setMatrix(matrix);
-			project.setXcoordinate(distance - 1);
+			Project project = new Project();
 
 			project.InputCommand();
 
-		}
+
 
 	}
 
